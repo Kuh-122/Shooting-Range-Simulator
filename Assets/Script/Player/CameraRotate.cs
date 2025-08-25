57ed958f830d73bfe770e4d1144400fd7ef28bef
Kuh-122 <Kimeh20091021@gmail.com>
2025-08-25 20:08:10 +0900

    카메라 회전 기능 구현

--- Assets/Script/Player/CameraRotate.cs
+++ Assets/Script/Player/CameraRotate.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class CameraRotate : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float rotCamXAxisSpeed = 5; // 수평감도
+    [SerializeField]
+    private float rotCamYAxisSpeed = 5; // 수직감도
 
-    // Update is called once per frame
-    void Update()
+    private float limitMinX = -50; // X축 회전 최소
+    private float limitMaxX = 50; // X축 회전 최대
+    private float eulerAngleX;
+    private float eulerAngleY;
+
+    public void UpdateRotate(float mouseX, float mouseY)
     {
-        
+        eulerAngleY += mouseX * rotCamYAxisSpeed;
+        eulerAngleX += mouseY * rotCamXAxisSpeed;
+
+        // 
+        eulerAngleX = ClampAngle(eulerAngleX, limitMinX, limitMaxX);
+
+        transform.rotation = Quaternion.Euler(eulerAngleX, eulerAngleY, 0);
+    }    
+
+private float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360) angle += 360;
+        if (angle > 360) angle -= 360;
+
+        return Mathf.Clamp(angle, min, max);
     }
 }
